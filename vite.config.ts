470f8f94f784daea4ea24f5643ed83cd75a53c3f
Eduhow <eduhow@github.com>
2026-04-20 18:38:09 +0300

    Fix base path for root deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/2/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
